a6d6913785a0c187961d5688c9e5824081dfc583
Unknown <biggstd@gmail.com>
2018-10-02 10:34:47 -0700

    Updated entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-echo "Not implemented, the application names must be filled in."
-#bokeh serve\
-#  --port 5006\
-#  --use-xheaders\
-#  --address 0.0.0.0\
-#  --allow-websocket-origin=130.20.47.245:8123\
-#  --allow-websocket-origin localhost:8123\
-#  --allow-websocket-origin idreamvisualization.pnl.gov\
-#  --allow-websocket-origin lampdev02.pnl.gov:8123\
+
+echo "Starting Bokeh server..."
+
+bokeh serve\
+  --port 5006\
+  --use-xheaders\
+  --address 0.0.0.0\
+  --allow-websocket-origin=130.20.47.245:8123\
+  --allow-websocket-origin localhost:8123\
+  --allow-websocket-origin idreamvisualization.pnl.gov\
+  --allow-websocket-origin lampdev02.pnl.gov:8123\
+  /opt/bkapps/scatter\
+  /opt/bkapps/table
